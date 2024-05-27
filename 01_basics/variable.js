a546b5fc08ea952d8af7a100e06b78278f97f25c
abishekluitel <abishekluitel2@gmail.com>
2024-05-27 11:46:36 +0545

    about variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+//Variable is a continer which hold the values, that could be Text, number true/fasle and any another complex DataTransfer.
+
+// variable name must start with the letters or _ or $
+// we can use _ and $ symbole any places of the variables.$
+// Always need to follow the camelCase Indentation for variables.
+// White space characters are not allowed.
+
+// const var let 
+
+const userId = 1;
+var userName = "Abishek Luitel";
+let userEmail = "abishekluitel2@gmail.com";
+let userState = null;
+let userAddress;
+
+console.table([typeof userName, userName, typeof userEmail, userEmail, typeof userState, userState, typeof userAddress, userAddress]);

acf065cdcef2a8887d99be61677c1c20253c47a8
abishekluitel <abishekluitel2@gmail.com>
2024-05-27 12:35:42 +0545

    about data-types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // Use New java script engine to validate the codes.
+// Dattypes decide the type of values decleared in the variables.
+/*Premitive data Types
+1. number: Represent the number values till 2 power 53.
+2. bigInt: In huge data like stock markets, trding companys.
+3. Strings: Everthings that comes inside the double or single qoute.
+4. bollean: true or false.
+5. undefined: Value that is not defined.
+6. null: value that is defined but empty.
+7. symbol: unique.
+
+Objects:
+*/
+console.log(typeof "Abishek");
